9aaa2c2b483577a6c8551aaebb2891aa7dd9e1ce
enesakar <enesakar@gmail.com>
2021-12-07 15:32:37 -0800

    cf examples

--- using-cloudflare-workers/index.js
+++ using-cloudflare-workers/index.js
@@ -1,3 +1,6 @@
+import { auth,get } from '@upstash/redis'
+
+auth(UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN)
 
 addEventListener("fetch", event => {
     event.respondWith(handleRequest(event.request))
@@ -5,9 +8,6 @@ addEventListener("fetch", event => {
 
 async function handleRequest(request) {
     let country = new Map(request.headers).get("cf-ipcountry");
-    let url = `https://usw1-selected-termite-30690.upstash.io/get/${country}?_token=AnfiASQgNWQzYmYzMDYtZTJkOS00YWQxLTlhZDAtZmUyOTc5ZGVlOTNlrtzXPqVS39LBiT81oCue0Bal1BuC2CnlMhOJX9Odts4=`;
-    let res = await fetch(url);
-    let restext = await res.text();
-    let greeting = await JSON.parse(restext);
-    return greeting.result ? new Response(greeting.result) : new Response("Hello!");
+    let greeting = await get(country);
+    return greeting.data ? new Response(greeting.data) : new Response("Hello!");
 }
