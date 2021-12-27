e9c3a4b8a3f1c01b73f52d48a34015eea09aa63b
enesakar <enesakar@gmail.com>
2021-12-27 14:23:38 -0800

    nextjs waiting room

---
+++ nextjs-waiting-room/pages/api/_middleware.ts
@@ -0,0 +1,112 @@
+import {auth, dbsize, get, setex} from '@upstash/redis';
+import {NextFetchEvent, NextRequest, NextResponse} from 'next/server';
+
+const COOKIE_NAME_ID = '__waiting_room_id'
+const COOKIE_NAME_TIME = '__waiting_room_last_update_time'
+const UPSTASH_REDIS_REST_TOKEN = "REPLACE_HERE"
+const UPSTASH_REDIS_REST_URL = "REPLACE_HERE"
+const TOTAL_ACTIVE_USERS = 10
+const SESSION_DURATION_SECONDS = 30
+
+auth(UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN);
+
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+    const cookies = req.cookies
+    let userId
+    if (cookies[COOKIE_NAME_ID] != null) {
+        userId = cookies[COOKIE_NAME_ID]
+    } else {
+        userId = makeid(8)
+    }
+
+    let res = await dbsize()
+    console.log('current capacity:' + res.data)
+    // there is enough capacity
+    if (res.data < TOTAL_ACTIVE_USERS) {
+        return getDefaultResponse(req, userId)
+    } else {  // site capacity is full
+        let obj2 = await get(userId)
+        if (obj2.data === '1') { // the user has already active session
+            return getDefaultResponse(req, userId)
+        } else { // capacity is full so the user is forwarded to waiting room
+            return getWaitingRoomResponse()
+        }
+    }
+}
+
+
+function makeid(length: number) {
+    var result = ''
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    var charactersLength = characters.length
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength))
+    }
+    return result
+}
+
+
+async function getDefaultResponse(request: NextRequest, userId: string) {
+    // uncomment below to test the function with a static html content
+    let newResponse = new NextResponse(default_html);
+    newResponse.headers.set('content-type', 'text/html;charset=UTF-8')
+
+    // const response = await fetch(request)
+    // const newResponse = new Response(response.body, response)
+
+    const cookies = request.cookies;
+    const now = Date.now()
+    let lastUpdate = cookies[COOKIE_NAME_TIME]
+    let lastUpdateTime = 0
+    if (lastUpdate)
+        lastUpdateTime = parseInt(lastUpdate)
+    const diff = now - lastUpdateTime
+    const updateInterval = SESSION_DURATION_SECONDS * 1000 / 2
+    if (diff > updateInterval) {
+        await setex(userId, SESSION_DURATION_SECONDS, "1")
+        newResponse.cookie(COOKIE_NAME_TIME, now.toString());
+    }
+    newResponse.cookie(COOKIE_NAME_ID, userId);
+    return newResponse
+}
+
+async function getWaitingRoomResponse() {
+    const newResponse = new NextResponse(waiting_room_html);
+    newResponse.headers.set('content-type', 'text/html;charset=UTF-8')
+    return newResponse
+}
+
+const waiting_room_html = `
+<title>Waiting Room</title>
+<meta http-equiv='refresh' content='30' />
+
+<style>*{box-sizing:border-box;margin:0;padding:0}body{line-height:1.4;font-size:1rem;font-family:ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif;padding:2rem;display:grid;place-items:center;min-height:100vh}.container{width:100%;max-width:800px}p{margin-top:.5rem}</style>
+
+<div class='container'>
+  <h1>
+    <div>You are now in line.</div>
+    <div>Thanks for your patience.</div>
+  </h1>
+  <p>We are experiencing a high volume of traffic. Please sit tight and we will let you in soon. </p>
+  <p><b>This page will automatically refresh, please do not close your browser.</b></p>
+</div>
+`
+
+
+const default_html = `
+<title>Waiting Room Demo</title>
+
+<style>*{box-sizing:border-box;margin:0;padding:0}body{line-height:1.4;font-size:1rem;font-family:ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif;padding:2rem;display:grid;place-items:center;min-height:100vh}.container{width:100%;max-width:800px}p{margin-top:.5rem}</style>
+
+<div class="container">
+  <h1>
+    <div>Waiting Room Demo</div>
+  </h1>
+    <p>
+              Visit this site from a different browser, you will be forwarded to the waiting room when the capacity is full.
+    </p>
+  <p>  Check <a href='//github.com/upstash/redis-examples/tree/master/nextjs-waiting-room' style={{"color": "blue"}}>this project </a> to set up a waiting room for your website.</p>
+</div>
+`
