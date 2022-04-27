2bb47d8cc4ad0a12694bfbc8e224455f63630ca0
enesakar <enesakar@gmail.com>
2022-04-27 16:30:48 -0700

    fly io

---
+++ using-fly-io/README.md
@@ -0,0 +1,10 @@
+# hellonode
+
+## A Fly Example
+
+This is a simple application used in the [fly.io Getting Started](https://fly.io/docs/getting-started/node/)  documentation showing how to deploy a Node application using Flyctl's builtin Nodejs deployment option.
+
+* Run flyctl init
+* When prompted for a builder, select builtin Nodejs.
+* Run flyctl deploy
+
---
+++ using-fly-io/server.js
@@ -0,0 +1,24 @@
+const {Redis} = require('@upstash/redis');
+
+const redis = new Redis({
+    url: 'REPLACE_HERE',
+    token: 'REPLACE_HERE',
+})
+
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get(["/", "/:name"], async (req, res) => {
+    let greeting = "<h1>Hello From Node on Fly!</h1>";
+    if (req.url !== "/favicon.ico") {
+        const data = await redis.incr('count');
+        res.send(greeting + "</br> Counter: " + data);
+    }   else {
+        res.send("");
+    }
+});
+
+app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`))
+
