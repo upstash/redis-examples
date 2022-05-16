00b117e4f319bd36b90e367927481933b5bb2693
Adem ilter <ademilter@gmail.com>
2022-05-16 14:36:58 +0300

    fix ui

--- next-caching-with-redis/pages/index.tsx
+++ next-caching-with-redis/pages/index.tsx
@@ -15,36 +15,36 @@ export default function Home() {
 
   return (
     <main className="text-center">
-      <h1 className="text-4xl font-bold">Covid Tracker</h1>
+      <h1 className="text-6xl font-bold">Covid Tracker</h1>
 
-      <p className="mt-4 text-xl">Top 10 countries with the most cases today</p>
+      <p className="mt-4 text-2xl text-gray-500">
+        Top 10 countries with the most cases today
+      </p>
 
-      <div className="mt-16">
-        <div className="" onClick={refresh}>
-          <table className="m-auto table-fixed text-left">
-            <thead>
-              <tr className="border-b-2">
-                <th className="py-1 px-2">Country</th>
-                <th className="py-1 px-2 text-right">Today Cases</th>
-                <th className="py-1 px-2 text-right">Today Deaths</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.data.map((item: any) => (
-                <tr className="border-b">
-                  <td className="py-1 px-2">{item.country}</td>
-                  <td className="py-1 px-2 text-right">{item.todayCases}</td>
-                  <td className="py-1 px-2 text-right">{item.todayDeaths}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <br />
-          <em>
-            Loaded from {data.type} in <b>{data.latency}</b> milliseconds. Click
-            to reload.
-          </em>
-        </div>
+      <table className="mt-20 w-full text-left">
+        <thead>
+          <tr className="border-b-2">
+            <th className="py-1 px-2">Country</th>
+            <th className="py-1 px-2 text-right">Today Cases</th>
+            <th className="py-1 px-2 text-right">Today Deaths</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.data.map((item: any) => (
+            <tr className="border-b">
+              <td className="py-1 px-2">{item.country}</td>
+              <td className="py-1 px-2 text-right">{item.todayCases}</td>
+              <td className="py-1 px-2 text-right">{item.todayDeaths}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className="mt-10 rounded bg-primary-50 p-4 text-primary-900">
+        Loaded from {data.type} in <b>{data.latency}</b> milliseconds.{" "}
+        <span className="cursor-pointer underline" onClick={refresh}>
+          Click to reload.
+        </span>
       </div>
     </main>
   );
