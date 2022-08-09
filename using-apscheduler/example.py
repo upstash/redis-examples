2313abbcbe75464563c33e6714c3b7e48b8cad9f
burak-upstash <burak@upstash.com>
2022-08-09 18:02:43 +0300

    fix test

--- using-apscheduler/example.py
+++ using-apscheduler/example.py
@@ -1,12 +1,17 @@
-import os 
+import os
 from datetime import datetime
 
 from apscheduler.jobstores.redis import RedisJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 from apscheduler.schedulers.background import BackgroundScheduler
 
+counter = 0
+
 def tick():
     print('Execution time has come: %s' % datetime.now())
+    global counter
+    counter += 1
+    print('counter:', counter)
 
 if __name__ == '__main__':
 
@@ -14,9 +19,10 @@ if __name__ == '__main__':
     password = os.environ['UPSTASH_REDIS_PASSWORD']
     port = os.environ['UPSTASH_REDIS_PORT']
 
-
     jobstores = {
-        'default': RedisJobStore(jobs_key='dispatched_trips_jobs', run_times_key='dispatched_trips_running', host=host, port=port, password=password)
+        'default': RedisJobStore(jobs_key='jobs', run_times_key='running', host=host, port=port, password=password)
+        # 'default': RedisJobStore(jobs_key='jobs', run_times_key='running', host='localhost', port=6379)
+
     }
     executors = {
         'default': ThreadPoolExecutor(100),
@@ -24,10 +30,16 @@ if __name__ == '__main__':
     }
 
     scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors)
+    # scheduler = BackgroundScheduler(job_defaults={'misfire_grace_time': 2}, jobstores=jobstores, executors=executors)
 
-    scheduler.add_job(tick, 'interval', seconds=3)
+    # job = scheduler.add_job(tick, 'interval', seconds=5, id='test-function')
 
     scheduler.start()
 
-    while True:
+    # while True:
+    #     # time.sleep(1000)
+    #     pass
+
+    counter = 1
+    while True and counter < 10:
         pass
