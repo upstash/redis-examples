fdb23724427ac7047cf399979add01cdf1b56715
burak-upstash <burak@upstash.com>
2022-08-24 14:54:21 +0300

    fix

--- using-apscheduler/test.py
+++ using-apscheduler/test.py
@@ -1,6 +1,5 @@
 from main import scheduler
 
-scheduler.remove_all_jobs()
 counter = 0
 steps = 0
 
@@ -18,13 +17,14 @@ def decrement_val():
 
 
 if __name__ == '__main__':
+    scheduler.remove_all_jobs()
     len_initial = len(scheduler.get_jobs())
 
     id_inc = 'test_inc'
     id_dec = 'test_dec'
 
-    scheduler.add_job(increment_val, 'interval', seconds=1, id=id_inc)
-    scheduler.add_job(decrement_val, 'interval', seconds=2, id=id_dec)
+    scheduler.add_job(increment_val, 'interval', seconds=1, id=id_inc, misfire_grace_time=5)
+    scheduler.add_job(decrement_val, 'interval', seconds=2, id=id_dec, misfire_grace_time=5)
 
     len_after = len(scheduler.get_jobs())
 
