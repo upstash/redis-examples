8da1d74d36304d387b6a55c9d72252b09904d2ae
burak-upstash <burak@upstash.com>
2022-08-25 17:34:33 +0300

    test

--- using-apscheduler/test.py
+++ using-apscheduler/test.py
@@ -21,6 +21,7 @@ if __name__ == '__main__':
     scheduler.start()
     scheduler.remove_all_jobs()
 
+    time.sleep(4)
     len_initial = len(scheduler.get_jobs())
     print("len_initial:", len_initial)
 
@@ -30,13 +31,14 @@ if __name__ == '__main__':
     scheduler.add_job(increment_val, 'interval', seconds=1, id=id_inc)
     scheduler.add_job(decrement_val, 'interval', seconds=2, id=id_dec)
 
+    time.sleep(4)
     len_after = len(scheduler.get_jobs())
 
     print("len_after:", len_after)
 
 
-    # if len_after != (len_initial + 2):
-    #     raise Exception("Jobs haven't been scheduled.")
+    if len_after != (len_initial + 2):
+        raise Exception("Jobs haven't been scheduled.")
 
     while counter < 5:
         pass
